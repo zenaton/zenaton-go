dd97e49890f38f8c275ddd12601bcad6e8dc308e
Michael Kurrels <mjkurrels@gmail.com>
2018-10-10 16:14:09 +0200

    Uncomment suit test

--- v1/zenaton/service/serializer/zenaton_suite_test.go
+++ v1/zenaton/service/serializer/zenaton_suite_test.go
@@ -1,14 +1,14 @@
 package serializer_test
 
-//
-//import (
-//	. "github.com/onsi/ginkgo"
-//	. "github.com/onsi/gomega"
-//
-//	"testing"
-//)
-//
-//func TestZenaton(t *testing.T) {
-//	RegisterFailHandler(Fail)
-//	RunSpecs(t, "Zenaton Suite")
-//}
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"testing"
+)
+
+func TestZenaton(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Zenaton Suite")
+}
